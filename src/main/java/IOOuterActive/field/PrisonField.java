adf988ba219a889bfd088d8df2d7f32d96ff98da
David Mikkelsen <Davidmikkelsen@live.dk>
2019-11-18 15:43:20 +0100

    Add PrisonField

---
+++ src/main/java/IOOuterActive/field/PrisonField.java
@@ -0,0 +1,39 @@
+package IOOuterActive.field;
+
+/**
+ *
+ * @author David Mikkelsen
+ * @version 1.0
+ */
+public class PrisonField extends Field {
+
+    private boolean convicted;
+
+    public PrisonField(String name, int id){
+        super(name,id);
+    }
+
+    /**
+     * Imprisons a player
+     */
+    public void imPrison(){
+
+        convicted = true;
+
+    }
+
+    /**
+     * Releases a player from prison
+     */
+    public void release(int bail, String card){
+
+        if(bail > 0)
+            convicted = false;
+        else if(card.equals("Release"))
+            convicted = false;
+
+
+    }
+
+
+}
