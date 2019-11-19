673cbedbcc7e1d2b89be0785b4e5ad251c001e00
TimJakobsen <tim.jakobsen98@gmail.com>
2019-11-19 02:10:31 +0100

    Added chancecards

---
+++ src/main/java/IOOuterActive/Card.java
@@ -0,0 +1,14 @@
+package IOOuterActive;
+
+public class Card {
+    private String name, text;
+
+    public Card(String name, String text) {
+        this.name = name;
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
