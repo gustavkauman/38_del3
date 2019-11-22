250a2688d5aba23600f53d01c38cae94277dae04
Gustav Utke Kauman <gustav.kauman@gmail.com>
2019-11-22 13:55:08 +0100

    Re-add chance card logic

--- src/main/java/IOOuterActive/controller/GameController.java
+++ src/main/java/IOOuterActive/controller/GameController.java
@@ -14,12 +14,14 @@ public class GameController {
     private Language lang;
     private GameBoard gb;
     private MatadorJuniorGUI out;
+    private CardBundle cardBundle = new CardBundle();
     private Player[] players;
 
     public GameController() throws IOException {
         this.lang = new Language();
         this.lang.readFile();
         this.gb = new GameBoard();
+        this.cardBundle.createCards();
 
         GUI gui = createGUIBoard();
         out = new MatadorJuniorGUI(gui, lang);
@@ -122,6 +124,115 @@ public class GameController {
 
         return new GUI(guiFields);
 
+    }
+
+    private void drawCard(Player currentPlayer){
+        Card drawnCard = cardBundle.getCard();
+        System.out.println(drawnCard.getText());
+
+        switch (drawnCard.getName()){
+            case "GiveToCar":
+                //Insert logic here
+                players[0].setPlayerSpecificCard(true);
+                drawCard(currentPlayer);
+                break;
+            case "GiveToShip":
+                players[1].setPlayerSpecificCard(true);
+                drawCard(currentPlayer);
+                break;
+            case "GiveToCat":
+                if(players.length > 2)
+                    players[2].setPlayerSpecificCard(true);
+                drawCard(currentPlayer);
+                break;
+            case "GiveToDog":
+                if(players.length > 3)
+                    players[3].setPlayerSpecificCard(true);
+                drawCard(currentPlayer);
+                break;
+            case "GoToStart":
+                checkStartPassed(24 - currentPlayer.getCurrentField(), currentPlayer);
+                break;
+            case "Move5":
+                int chosenNumberOfMoves;
+                //TODO: int chosenNumberOfMoves = guiController ...
+                //checkStartPassed(chosenNumberOfMoves);
+                //TODO: Udfør feltets funktion
+                break;
+            case "OrangeFree":
+
+                break;
+            case "MoveOrCard":
+                boolean move = false;
+                //TODO: move = guiController getUserSelection ...
+                if(move){
+                    checkStartPassed(1, currentPlayer);
+                    //TODO: Udfør feltets funktion
+                } else {
+                    drawCard(currentPlayer);
+                }
+                break;
+            case "Overeating":
+                currentPlayer.addMoney(-2);
+                break;
+            case "OrangeOrGreenFree":
+
+                break;
+            case "LightblueFree":
+
+                break;
+            case "GetOutOfJail":
+                currentPlayer.setJailCards(currentPlayer.getJailCards() + 1);
+                break;
+            case "MoveToStrand":
+                checkStartPassed(23 - currentPlayer.getCurrentField(), currentPlayer);
+                //TODO: Udfør feltets funktion
+                break;
+            case "Birthday":
+                for(Player player : players){
+                    if(player != currentPlayer){
+                        player.addMoney(-1);
+                        currentPlayer.addMoney(1);
+                    }
+                }
+                break;
+            case "PinkOrDarkblueFree":
+
+                break;
+            case "Homework":
+                currentPlayer.addMoney(2);
+                break;
+            case "RedFree":
+
+                break;
+            case "Skatepark":
+                int movedFields;
+                if(currentPlayer.getCurrentField() < 10){
+                    movedFields = 10 - currentPlayer.getCurrentField();
+                } else {
+                    movedFields = 24 - (currentPlayer.getCurrentField() - 10);
+                }
+                checkStartPassed(movedFields, currentPlayer);
+                //TODO: Udfør feltets funktion.
+                break;
+            case "LightblueOrRedFree":
+
+                break;
+            case "BrownOrYellowFree":
+                break;
+        }
+    }
+
+    private void checkStartPassed(int fieldsMoved, Player currentPlayer) {
+        if(currentPlayer.getCurrentField() + fieldsMoved < 24){
+            currentPlayer.setCurrentField(currentPlayer.getCurrentField() + fieldsMoved);
+            //TODO: Update GUI
+        } else {
+            System.out.println("Du passerede START og modtager $2.");
+            currentPlayer.addMoney(2);
+            currentPlayer.setCurrentField(currentPlayer.getCurrentField() + fieldsMoved - 24);
+            //TODO: Update GUI
+        }
     }
 
 }
