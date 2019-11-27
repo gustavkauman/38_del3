aae318e0cc010f9c4f5f9f896992253f763f13ac
Sebastian_Bjerre <sebastianbjerre@hotmail.com>
2019-11-27 20:51:50 +0100

    R14 done and passes

--- src/test/java/IOOuterActive/game/GameTest.java
+++ src/test/java/IOOuterActive/game/GameTest.java
@@ -4,30 +4,26 @@ import IOOuterActive.entities.Player;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
-import java.security.PublicKey;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class GameTest {
 
     @Test
     public void r05_check_two_moves() {
         Player player = new Player();
         player.setCurrentField(20);
-        Game.checkStartPassed(7,player);
-        Game.checkStartPassed(5,player);
+        Game.checkStartPassed(7, player);
+        Game.checkStartPassed(5, player);
         int actual = player.getCurrentField();
         int expected = 8;
-        Assert.assertEquals(actual,expected);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void r06_check_start_passed() {
         Player player = new Player();
         player.setCurrentField(20);
-        boolean actual = Game.checkStartPassed(4,player);
+        boolean actual = Game.checkStartPassed(4, player);
         boolean expected = true;
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
@@ -35,32 +31,58 @@ class GameTest {
         Player player = new Player();
         player.setCurrentField(20);
         player.setMoney(10);
-        Game.checkStartPassed(4,player);
+        Game.checkStartPassed(4, player);
         int actual = player.getMoney();
         int expected = 12;
-        Assert.assertEquals(actual,expected);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
-    public void r06_check_money_passing_start() {
+    public void r06_check_money_passing_start_1000_times() {
         Player player = new Player();
         player.setCurrentField(20);
         player.setMoney(10);
-        Game.checkStartPassed(6,player);
+        for (int i = 0; i <= 1000; i++) {
+            Game.checkStartPassed(24, player);
+        }
         int actual = player.getMoney();
-        int expected = 12;
-        Assert.assertEquals(actual,expected);
+        int expected = 2012;
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
-    public void r06_check_no_money_not_on_or_passing_start(){
+    public void r06_check_no_money_not_on_or_passing_start() {
         Player player = new Player();
         player.setCurrentField(20);
         player.setMoney(10);
-        Game.checkStartPassed(3,player);
+        Game.checkStartPassed(3, player);
         int actual = player.getMoney();
         int expected = 10;
-        Assert.assertEquals(actual,expected);
+        Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void r14_check_if_same_position_after_1_full_board_rotation() {
+        Player player = new Player();
+        player.setCurrentField(20);
+        player.setMoney(10);
+        Game.checkStartPassed(24, player);
+        int actual = player.getCurrentField();
+        int expected = 20;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void r14_check_same_position_after_1000_full_board_rotations() {
+        Player player = new Player();
+        player.setCurrentField(20);
+        for (int i = 0; i <= 1000; i++) {
+            Game.checkStartPassed(24, player);
+        }
+        int actual = player.getCurrentField();
+        int expected = 20;
+        Assert.assertEquals(expected, actual);
+    }
+
 
 }
