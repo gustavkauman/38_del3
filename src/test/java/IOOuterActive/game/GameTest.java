0b06b53a0d09b01377bd817f6e9452b91f0c9a7d
Sebastian_Bjerre <sebastianbjerre@hotmail.com>
2019-11-27 19:56:53 +0100

    new testbranch for R5 & R6

---
+++ src/test/java/IOOuterActive/game/GameTest.java
@@ -0,0 +1,18 @@
+package IOOuterActive.game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    public void r05_check_money_on_start() {
+        Game game = new Game();
+        game.
+
+
+
+    }
+
+}
