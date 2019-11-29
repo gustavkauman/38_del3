62f64af613292ba4f1ed757976b66d9e0f62ad1d
Sebastian_Bjerre <sebastianbjerre@hotmail.com>
2019-11-29 10:46:59 +0100

    tests merged into dev

--- src/main/java/IOOuterActive/Main.java
+++ src/main/java/IOOuterActive/Main.java
@@ -6,39 +6,12 @@ import IOOuterActive.game.Game;
 import java.io.IOException;
 import java.util.Map;
 
-public class Main {
+public class
+Main {
     public static void main(String[] args) throws IOException {
 
         GameController controller = new GameController();
         controller.playGame();
-
-        /*
-        DiceCup dg = new DiceCup(6,2);
-        System.out.println(dg.getSum());
-        dg.throwDice();
-        System.out.println(dg.getSum());
-
-
-        Language lang = new Language();
-        try {
-            lang.readFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Map<String, String> text = lang.getOutput();
-        System.out.println(text.get("TestKey"));
-        System.out.println(text.get("NyLinje"));
-        System.out.println(text.get("Spillehallen"));
-
-
-
-
-
-
-
-        Game game = new Game();
-        game.playGame();
-         */
+        
     }
 }
