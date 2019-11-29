736a96447084e85d06cd2dbf03002112080ae1c5
Moamal <moamal001@gmail.com>
2019-11-29 09:52:59 +0100

    Added javaDoc to the Junit test for the Player class

--- src/test/java/IOOuterActive/entities/PlayerTest.java
+++ src/test/java/IOOuterActive/entities/PlayerTest.java
@@ -7,6 +7,18 @@ import static org.junit.Assert.*;
 
 public class PlayerTest {
 
+    /**
+     * Testing the Player class. Only two methods are being tested on the basis og Getter and Setter
+     * methods test worth.
+     *
+     * @author Moamal Abdul-Mahdi
+     * @Version 1.0
+     */
+
+
+    /**
+     * Testing the methods that modifies the amount of money a player have
+     */
     @Test
     public void addMoney() {
         Player p = new Player();
@@ -14,6 +26,10 @@ public class PlayerTest {
         p.addMoney(2);
         assertEquals(4, p.getMoney());
     }
+
+    /**
+     * Testing the method that disqualifies a player if his/her money is 0 or below
+     */
 
     @Test
     public void isFallit() {
