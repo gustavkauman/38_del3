5687618051627a8d783f134706d0553b27629f53
Moamal <moamal001@gmail.com>
2019-11-29 08:55:33 +0100

    Added javaDoc to the Junit test

--- src/test/java/IOOuterActive/controller/R11.java
+++ src/test/java/IOOuterActive/controller/R11.java
@@ -6,15 +6,26 @@ import IOOuterActive.entities.Player;
 import IOOuterActive.entities.PropertyField;
 import IOOuterActive.game.Game;
 import org.junit.Test;
-
 import java.io.IOException;
-
 import static org.junit.Assert.*;
 
-public class R11 {
+/**
+ * Testing the R11. The test consists of two parts. The first part tests who the winner
+ * in the case that a player has more money than the rest of the players.
+ *
+ * The second part test who the player is when two players have the same amount of money
+ * but one of them has more properties.
+ *
+ * @author Moamal Abdul-Mahdi
+ * @Version 1.0
+ */
 
+public class R11 {
+    /**
+     * First part
+     */
     @Test
-    public void testerR11_1 () throws IOException {
+    public void testerR11_1 () {
         Player p1 = new Player();
         Player p2 = new Player();
         Player p3 = new Player();
@@ -30,8 +41,12 @@ public class R11 {
         assertEquals(p1, actual);
     }
 
+    /**
+     * Second part
+     */
+
     @Test
-    public void testerR11_2 () throws IOException {
+    public void testerR11_2 () {
         Player p1 = new Player();
         Player p2 = new Player();
         Player p3 = new Player();
