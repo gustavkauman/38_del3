e61b55cf3419b84bf22165ec6959fb3b57e574c0
Sebastian_Bjerre <sebastianbjerre@hotmail.com>
2019-11-29 10:58:12 +0100

    Moved R05 to TC01

--- src/test/java/IOOuterActive/game/TC03.java
+++ src/test/java/IOOuterActive/game/TC03.java
@@ -17,49 +17,7 @@ class TC03 {
         Assert.assertEquals(expected, actual);
     }
 
-    @Test
-    public void r06_check_start_passed() {
-        Player player = new Player();
-        player.setCurrentField(20);
-        boolean actual = Game.checkStartPassed(4, player);
-        boolean expected = true;
-        Assert.assertEquals(expected, actual);
-    }
 
-    @Test
-    public void r06_check_money_on_start() {
-        Player player = new Player();
-        player.setCurrentField(20);
-        player.setMoney(10);
-        Game.checkStartPassed(4, player);
-        int actual = player.getMoney();
-        int expected = 12;
-        Assert.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void r06_check_money_passing_start_1000_times() {
-        Player player = new Player();
-        player.setCurrentField(20);
-        player.setMoney(10);
-        for (int i = 0; i <= 1000; i++) {
-            Game.checkStartPassed(24, player);
-        }
-        int actual = player.getMoney();
-        int expected = 2012;
-        Assert.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void r06_check_no_money_not_on_or_passing_start() {
-        Player player = new Player();
-        player.setCurrentField(20);
-        player.setMoney(10);
-        Game.checkStartPassed(3, player);
-        int actual = player.getMoney();
-        int expected = 10;
-        Assert.assertEquals(expected, actual);
-    }
 
     @Test
     public void r14_check_if_same_position_after_1_full_board_rotation() {
